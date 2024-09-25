8ac0da055c772810670b0ec316deddc821368129
shoaibkhalid111 <160658209+shoaibkhalid111@users.noreply.github.com>
2024-09-25 22:38:04 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/header/page'
+import About from './components/about/page'
+
+const Homepage = () => {
+  return (
+    <>
+      <Header/>
+      <About/>
+    </>
+  )
+}
+
+export default Homepage
